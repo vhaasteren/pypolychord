c093ab0a6a5d1a1756f95f9312071740e322e572
Lindley Lentati <ltl21@cam.ac.uk>
2015-02-13 20:42:26 +0000

    Added output and hierachy parameters to C interface, comments on hierarchy interface are in Initsampler.f90

--- CExample/polychord.h
+++ CExample/polychord.h
@@ -16,15 +16,15 @@ namespace chord
 {
 
         extern "C" {
-                void DOSAMPLING(double (*Lfunc)(int &nDims, double *theta, int &nDerived, double *phi, void *context), int &Ndim, int &nDerived, int &nLive, int &Nchords,  double *PriorsArray, char *Froot, void *context);
+                void DOSAMPLING(double (*Lfunc)(int &nDims, double *theta, int &nDerived, double *phi, void *context), int &Ndim, int &nDerived, int &nLive, int &Nchords,  double *PriorsArray, char *Froot, void *context, double *output, int &do_grades, int &maxgrade, int *grades, int *grade_repeats, int *hypercube_indices, int *physical_indices);
         }
 
-        static void Sample(double (*Lfunc)(int &nDims, double *theta, int &nDerived, double *phi, void *context), int &Ndim, int &nDerived, int &nLive, int &Nchords,  double *PriorsArray, char *Froot, void *context){
+        static void Sample(double (*Lfunc)(int &nDims, double *theta, int &nDerived, double *phi, void *context), int &Ndim, int &nDerived, int &nLive, int &Nchords,  double *PriorsArray, char *Froot, void *context, double *output, int &do_grades, int &maxgrade, int *grades, int *grade_repeats, int *hypercube_indices, int *physical_indices){
 
                 int i;
                 for (i = strlen(Froot); i < 100; i++) Froot[i] = ' ';
 
-                DOSAMPLING(Lfunc, Ndim, nDerived, nLive, Nchords, PriorsArray, Froot, context);
+                DOSAMPLING(Lfunc, Ndim, nDerived, nLive, Nchords, PriorsArray, Froot, context, output, do_grades, maxgrade, grades, grade_repeats, hypercube_indices, physical_indices);
         }
 
 }
